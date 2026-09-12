156724b010e361d7138e96f4114713647fbfa199
Divya1206112 <divyabhuvik7@gmail.com>
2026-09-12 18:56:00 +0530

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,13 @@
+var numb = 8;
+function IsoddorEven(number) {
+    if(number %2 ===0) 
+      {
+      return "Even";
+
+    }
+    else {
+        return "Odd";
+    }
+
+}
+ console.log(IsoddorEven(numb));
