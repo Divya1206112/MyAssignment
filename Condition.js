acb9c96e53c1a655b66065c395e60ae7ffdf3c90
Divya1206112 <divyabhuvik7@gmail.com>
2026-09-12 18:44:49 +0530

    Add files via upload

---
+++ Condition.js
@@ -0,0 +1,15 @@
+
+
+function launchBrowser(Browsername)
+{
+    if(Browsername==="Chrome")
+        {
+        console.log("Launching chrome  Browser");
+        }
+else
+{
+    console.log("otherwise");
+}
+    }
+
+launchBrowser("Chrome");
